c6f47d7d5b43cda9207a165a8a814a5d8416cc11
Justin Schmitz <jschmitz2@hawk.iit.edu>
2019-07-28 10:00:25 -0500

    update requirements.txt

--- rpi_python/gpio_interface.py
+++ rpi_python/gpio_interface.py
@@ -8,7 +8,7 @@ import digitalio
 # lcd_columns = 16
 # lcd_rows = 2
 #
-b1 = Button(15)
+b1 = Button(18)
 l1 = LED(23)
 b2 = Button(24)
 l2 = LED(25)
@@ -106,7 +106,7 @@ if __name__ == "__main__":
 
     # Buttons and LEDs:
 
-    # B1: Pin 10, GPIO 15
+    # B1: Pin 12, GPIO 18
     # L1: Pin 16, GPIO 23
 
     # B2: Pin 18, GPIO 24
